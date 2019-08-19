cbaa9597b0df00ee41b5acdc99bb4dad089ac4d3
Joe <a5135324@gmail.com>
2019-08-19 13:05:39 +0800

    update server code

--- server/aes/server.py
+++ server/aes/server.py
@@ -1,9 +1,7 @@
+import os
 import time
 import socket
 from Crypto.Cipher import AES
-
-def pad(s):
-    return s + str.encode((BS - len(s) % BS) * chr(BS - len(s) %BS))
 
 def unpad(s):
     return s[:-ord(s[len(s) - 1:])]
@@ -15,11 +13,6 @@ def create_server(host, port):
     print('Server now listening!!')
     return s
 
-def decrypt_data(cipher_data, w):
-    data = cipher.decrypt(cipher_data)
-    unpad_data = unpad(data)
-    w.write(unpad_data)
-
 def receive_data(s):
     while True:
         conn, addr = s.accept()
@@ -30,43 +23,47 @@ def receive_data(s):
         unpad_name = unpad(name)
     
         t0 = time.time()
-        #a = 0
         with open(unpad_name, 'wb') as w:
-          while True:
-                cipher_data = conn.recv(1024)
-                #a = a + 1
-                #print('len = ' + str(len(cipher_data)))
-                #if len(cipher_data) % BS != 0:
-                #    print(cipher_data)
-                
+            while True:
+                cipher_data = conn.recv(buf_size)
                 length = len(cipher_data)
                 if not cipher_data:
                     break
-                elif length == 1024:
-                    #decrypt_data(cipher_data, w)
-                    
+                elif length == buf_size:
                     data = cipher.decrypt(cipher_data)
                     unpad_data = unpad(data)
                     w.write(unpad_data)
                 else:
-                    other_data = conn.recv(1024 - length)
-                    mix_data = cipher_data + other_data
-                    #print('mix_data len = ' , len(mix_data))
-                    #decrypt_data(mix_data, w)
-                    data = cipher.decrypt(mix_data)
-                    unpad_data = unpad(data)
-                    w.write(unpad_data)
+                    while True:
+                        length = len(cipher_data)
+                        other_data = conn.recv(buf_size - length)
+                        if not other_data:
+                            data = cipher.decrypt(cipher_data)
+                            unpad_data = unpad(data)
+                            w.write(unpad_data)
+                            break
+                        else:
+                            cipher_data = cipher_data + other_data
+                            if len(cipher_data) == buf_size:
+                                data = cipher.decrypt(cipher_data)
+                                unpad_data = unpad(data)
+                                w.write(unpad_data)
+                                break
 
-        all_time = time.time() - t0 - 1 # - a * 0.1
+        all_time = time.time() - t0 - 1 
 
         print('Transfer file use ' + str(all_time) + ' seconds.')
         print('Finish')
+        if os.path.exists(unpad_name):
+            os.remove(unpad_name)
+            print('delete finish')
     
 if __name__ == "__main__":
     host = '0.0.0.0'
     port = 50000
     BS = AES.block_size
     key = 'This is my key!!'
+    buf_size = 153600 
     cipher = AES.new(key, AES.MODE_ECB)
     s = create_server(host, port)
     receive_data(s)
